97b9a476bf41c361ccb1e4325f6ba530437122d9
IDMendis <dinushimendisxp@gmail.com>
2025-10-20 20:26:07 +0530

    code corrected

--- Controllers/UserCotroller.js
+++ Controllers/UserCotroller.js
@@ -1,36 +1,38 @@
 const User = require('../Model/UserModel');
 
+// Get all users
 const getAllUsers = async (req, res) => {
-    let Users;
-    try{
-        Users = await User.find();
-    }catch(err){
+    let users;
+    try {
+        users = await User.find();
+    } catch (err) {
         console.log(err);
     }
 
-    //not found nam
-    if(!Users){
-        return res.status(404).json({message: "No Users found"});
-    }
-    //display all users
-    return res.status(200).json({Users});
-
-
-    const addUsers= async(req,res,next)=>{
-        const {name,gmail,age,password}= req.body;
-        let users;
-        try{
-            users= new users({name,gmail,age,password});
-            await users.save();
-
-        }catch(err){
-            console.log(err);
-        }
-        if(!users){
-            return res.status(500).json({message: "Unable to add users"});
-        }
+    if (!users) {
+        return res.status(404).json({ message: "No users found" });
     }
 
+    return res.status(200).json({ users });
+};
+
+// Add a new user
+const addUsers = async (req, res) => {
+    const { name, gmail, age, password } = req.body;
+    let user;
+    
+    try {
+        user = new User({ name, gmail, age, password });
+        await user.save();
+    } catch (err) {
+        console.log(err);
+    }
+
+    if (!user) {
+        return res.status(500).json({ message: "Unable to add user" });
+    }
+
+    return res.status(201).json({ user });
 };
 
 exports.getAllUsers = getAllUsers;
