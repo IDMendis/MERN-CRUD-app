68fb428c3733ca58618308736a126181ba82f299
IDMendis <dinushimendisxp@gmail.com>
2025-10-21 20:51:42 +0530

    implemented the hetUserById method

--- Controllers/UserCotroller.js
+++ Controllers/UserCotroller.js
@@ -32,8 +32,28 @@ const addUsers = async (req, res) => {
         return res.status(500).json({ message: "Unable to add user" });
     }
 
-    return res.status(201).json({ user });
+    return res.status(200).json({ user });
+
+   
 };
+
+ //get by ID
+    const getById = async (req,res,next )=>{
+        const id =req.params.id;
+
+        let user;
+        try{
+            user = await User.findById(id);
+        }catch (err){
+            console.log.apply(err);
+        }
+        //if not available users
+        if(!user){
+            return res.status(404).json({message:"No user found"});
+        }
+        return res.status(200).json({user}); //user available nam eyage details tika display krnwa
+    }
 
 exports.getAllUsers = getAllUsers;
 exports.addUsers = addUsers;
+exports.getById = getById;
